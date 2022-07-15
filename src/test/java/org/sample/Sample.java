a09b7b348fc52d27c755a82791bff35f70a0ea45
Vivek200798 <vivek.kamaraj2@gmail.com>
2022-07-15 11:27:12 +0530

    JUnit project

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -1,31 +1,84 @@
 package org.sample;
 
-import org.helper.BaseClass;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import java.util.Date;
 
-public class Sample extends BaseClass {
-	
-	public static void main(String[] args) {
-		
-		chromeBrowser();
-		launchUrl("https://www.facebook.com/");
-		maxWindow();
-		pagrUrl();
-		pageTitle();
-		
-		WebElement txtEmail = driver.findElement(By.id("email"));
-		fillTextBox(txtEmail, "vivek.kamaraj2@gmail.com");
-		
-		WebElement txtPass = driver.findElement(By.id("pass"));
-		fillTextBox(txtPass, "12345");
-		
-		WebElement btnLogin = driver.findElement(By.name("login"));
-		btnClick(btnLogin);
-		
-		closeBrowser();
-		
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+
+	public static WebDriver driver;
+
+	@Test
+
+	public void Testcase3() {
+
+		String title = driver.getTitle();
+		System.out.println(title);
+
 	}
-	
+
+	@Test
+
+	public void Testcase1() {
+
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+
+	}
+
+	@Test
+
+	public void Testcase2() {
+
+		driver.findElement(By.id("email")).sendKeys("vivek27");
+		driver.findElement(By.id("pass")).sendKeys("myfacebookacc");
+		driver.findElement(By.name("login")).click();
+
+	}
+
+	@Before
+
+	public void before() {
+
+		Date d = new Date();
+		System.out.println(d);
+
+	}
+
+	@After
+
+	public void after() {
+
+		Date d = new Date();
+		System.out.println(d);
+	}
+
+	@BeforeClass
+
+	public static void beforeclass() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+	}
+
+	@AfterClass
+
+	public static void afterclass() {
+
+		driver.close();
+
+	}
+
 }
-	
